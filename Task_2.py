19c55e09b32a5d708f10183bf59741c3e8714719
Volodymyr Sliusarenko <volodymyr.slusarenko@gmail.com>
2024-03-09 13:45:05 +0200

    completed

--- Task_2.py
+++ Task_2.py
@@ -1,1 +1,12 @@
-print("hello HW 3")
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    ticket = set()
+    while len(ticket) < quantity:
+        ticket.add(random.randint(min, max))
+    ticket_numbers = list(ticket)
+    ticket_numbers.sort()
+    return ticket_numbers
+    
+lottery_numbers = get_numbers_ticket(1, 35, 7)
+print("Ваші лотерейні числа:", lottery_numbers)
