0d530d4c40a69e8870bb13e738ae18153bfa5a17
Volodymyr Sliusarenko <volodymyr.slusarenko@gmail.com>
2024-03-09 12:37:57 +0200

    completed task 1

--- Task_1.py
+++ Task_1.py
@@ -1,1 +1,14 @@
-print('hello homework3')
+from datetime import datetime
+
+date = input('Please enter date YYYY-MM-DD: ')
+
+def get_days_from_today(date):
+    current_date = datetime.today()
+    try:
+        date_formated = datetime.strptime(date, "%Y-%m-%d")
+        print(current_date.toordinal() - date_formated.toordinal())
+    except ValueError:
+        print('Incorrect data format, please try again')
+
+get_days_from_today(date)
+
