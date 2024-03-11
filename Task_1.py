fcc957162202f99d32e0f75829edcf4d382bcd0a
Volodymyr Sliusarenko <volodymyr.slusarenko@gmail.com>
2024-03-11 18:55:16 +0200

    set return for func

--- Task_1.py
+++ Task_1.py
@@ -4,11 +4,14 @@ date = input('Please enter date YYYY-MM-DD: ')
 
 def get_days_from_today(date):
     current_date = datetime.today()
+    days_differnce = 0
     try:
         date_formated = datetime.strptime(date, "%Y-%m-%d")
-        print(current_date.toordinal() - date_formated.toordinal())
+        days_differnce = current_date.toordinal() - date_formated.toordinal()
     except ValueError:
         print('Incorrect data format, please try again')
+        days_differnce = None
+    return days_differnce
 
-get_days_from_today(date)
+print(get_days_from_today(date))
 
