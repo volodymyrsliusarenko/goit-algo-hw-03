fb72fc8daed711a28da9e030fe85d511f1235099
Volodymyr Sliusarenko <volodymyr.slusarenko@gmail.com>
2024-03-11 19:32:00 +0200

    edits

--- Task_2.py
+++ Task_2.py
@@ -3,10 +3,14 @@ import random
 def get_numbers_ticket(min, max, quantity):
     ticket = set()
     while len(ticket) < quantity:
-        ticket.add(random.randint(min, max))
-    ticket_numbers = list(ticket)
-    ticket_numbers.sort()
+        if min < max and quantity <= (max - min):
+            ticket.add(random.randint(min, max))
+            ticket_numbers = list(ticket)
+            ticket_numbers.sort()
+        else:
+            ticket_numbers = []
+            break      
     return ticket_numbers
     
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(7, 15, 9)
 print("Ваші лотерейні числа:", lottery_numbers)
